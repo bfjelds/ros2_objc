46ec4f8ce7d9c010dcc9b21e5601307621fccde3
Esteve Fernandez <esteve@apache.org>
2017-04-14 22:47:03 +0200

    Streamline repos and generators for Travis

--- build_travis_macos.sh
+++ build_travis_macos.sh
@@ -2,14 +2,15 @@
 
 set -e
 
+git checkout $TRAVIS_BRANCH
+
 mkdir -p ~/ros2_objc_ws/src
 cd ~/ros2_objc_ws
-wget https://raw.githubusercontent.com/esteve/ros2_objc/master/ros2_objc_macos.repos
-vcs import ~/ros2_objc_ws/src < ros2_objc_macos.repos
+vcs import ~/ros2_objc_ws/src < $TRAVIS_BUILD_DIR/ros2_objc_macos_travis.repos
 cd ~/ros2_objc_ws/src/ros2_objc/ros2_objc
 git checkout $TRAVIS_BRANCH
 cd ~/ros2_objc_ws/src/ros2/rosidl_typesupport
-patch -p1 < ../../ros2_objc/ros2_objc/rosidl_typesupport_ros2_objc.patch
+patch -p1 < ../../ros2_objc/ros2_objc/rosidl_typesupport_ros2_objc_travis.patch
 cd ~/ros2_objc_ws
 src/ament/ament_tools/scripts/ament.py build --use-xcode --symlink-install --isolated | tee /tmp/ament_build.log | grep -v "/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/clang"
 tail /tmp/ament_build.log
