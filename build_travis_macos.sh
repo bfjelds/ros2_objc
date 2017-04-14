f5bad0259df5ad0333e09dc38395fc762826bc86
Esteve Fernandez <esteve@apache.org>
2017-04-14 15:38:00 +0200

    Move build code to separate script

---
+++ build_travis_macos.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+set -e
+
+mkdir -p ~/ros2_objc_ws/src
+cd ~/ros2_objc_ws
+wget https://raw.githubusercontent.com/esteve/ros2_objc/master/ros2_objc_macos.repos
+vcs import ~/ros2_objc_ws/src < ros2_objc_macos.repos
+cd ~/ros2_objc_ws/src/rosidl_typesupport
+patch -p1 < ../../ros2_java/ros2_java/rosidl_typesupport_ros2_objc.patch
+cd ~/ros2_objc_ws
+src/ament/ament_tools/scripts/ament.py build --use-xcode --symlink-install --isolated
