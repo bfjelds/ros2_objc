2950e9e77ba975e849186a6b122317d3b8c9917b
Esteve Fernandez <esteve@apache.org>
2017-04-14 15:45:03 +0200

    Fix

--- build_travis_macos.sh
+++ build_travis_macos.sh
@@ -6,7 +6,7 @@ mkdir -p ~/ros2_objc_ws/src
 cd ~/ros2_objc_ws
 wget https://raw.githubusercontent.com/esteve/ros2_objc/master/ros2_objc_macos.repos
 vcs import ~/ros2_objc_ws/src < ros2_objc_macos.repos
-cd ~/ros2_objc_ws/src/rosidl_typesupport
+cd ~/ros2_objc_ws/src/ros2/rosidl_typesupport
 patch -p1 < ../../ros2_java/ros2_java/rosidl_typesupport_ros2_objc.patch
 cd ~/ros2_objc_ws
 src/ament/ament_tools/scripts/ament.py build --use-xcode --symlink-install --isolated
