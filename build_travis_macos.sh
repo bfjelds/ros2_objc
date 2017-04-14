d39e033a1486b99dd5fecdc8945fabc36fd2a342
Esteve Fernandez <esteve@apache.org>
2017-04-14 18:52:12 +0200

    Fix

--- build_travis_macos.sh
+++ build_travis_macos.sh
@@ -6,6 +6,8 @@ mkdir -p ~/ros2_objc_ws/src
 cd ~/ros2_objc_ws
 wget https://raw.githubusercontent.com/esteve/ros2_objc/master/ros2_objc_macos.repos
 vcs import ~/ros2_objc_ws/src < ros2_objc_macos.repos
+cd ~/ros2_objc_ws/src/ros2_objc/ros2_objc
+git checkout $TRAVIS_BRANCH
 cd ~/ros2_objc_ws/src/ros2/rosidl_typesupport
 patch -p1 < ../../ros2_objc/ros2_objc/rosidl_typesupport_ros2_objc.patch
 cd ~/ros2_objc_ws
